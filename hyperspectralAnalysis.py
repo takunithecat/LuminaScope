ff378607e41b881e45dc48e2cca8d78fc65ee4e2
Daniel Wang <dan-wang@live.com>
2023-12-21 22:58:29 -0800

    color channel isolation added

--- hyperspectralAnalysis.py
+++ hyperspectralAnalysis.py
@@ -40,12 +40,26 @@ cv.imshow('Autumn', img)
 #     capture.set(4,height)
 
 # Isolate blue, green. red image
-# resulting images show concentration/intensity of r,b,g values
-b,g,r = cv.split(img)
+# Resulting grayscale images show concentration/intensity of r,b,g values
+# b,g,r = cv.split(img)
 
-cv.imshow('Blue', b)
-cv.imshow('Green', g)
-cv.imshow('Red', r)
+# cv.imshow('Blue', b)
+# cv.imshow('Green', g)
+# cv.imshow('Red', r)
+
+# Merging images from different channels
+# merge = cv.merge([b,g,r])
+# cv.imshow('Merged', merge)
+
+# Reconstructing images to show color images that show concentration of r,g,b
+# blank = np.zeros(img.shape[:2], dtype='uint8')
+# blue = cv.merge([b,blank,blank])
+# green = cv.merge([blank,g,blank])
+# red = cv.merge([blank,blank,r])
+
+# cv.imshow('Blue', blue)
+# cv.imshow('Green', green)
+# cv.imshow('Red', red)
 
 # Calculate histogram of color? levels
 
