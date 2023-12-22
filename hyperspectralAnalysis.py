301fcca5c55247d3f0b93f9528bc545a574d3e02
Daniel Wang <dan-wang@live.com>
2023-12-21 16:54:31 -0800

    added todo tasks as comments

---
+++ hyperspectralAnalysis.py
@@ -0,0 +1,26 @@
+import numpy
+import cv2
+import tifffile
+
+# This is an sample program for performing hyperspectral analysis using the LuminaScope system.
+# This software is distributed under the Apache 2.0 License.
+# For this analysis we need the three main libraries above.
+# They are numpy, opencv-python, and tifffile in that order.
+
+# Opening tiff file from file location
+
+# Isolate red image
+
+# Isolate blue image
+
+# Isolate green image
+
+# Calculate histogram of color? levels
+
+# Crop image
+
+# Smooth image
+
+# Threshhold image
+
+# Hyperspectral analysis
