bc832e4857b5992f6b64f6447ef2cccc2bf41d93
Daniel Wang <dan-wang@live.com>
2024-01-11 18:13:49 -0800

    rgb

--- calibrationTest.py
+++ calibrationTest.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 import os
 
 # calibration python script
-# -open image of coloured boxes
-# -isolate every box
+# - open image of coloured boxes
+# - isolate every box
 # - calculate the phasor transformation of every box
-# -and plot the mean  G and mean S coordinates of every box in a phasor plot
+# - and plot the mean  G and mean S coordinates of every box in a phasor plot
 
 # assign directory
 directory = 'Images'
@@ -20,6 +20,45 @@ def rescaleFrame(frame, scale=0.5):
 
     dimensions = (width, height)
     return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+# define phasor plot function
+def phasor(Gsmt):
+    fft=np.fft.fft(Gsmt, axis=2)
+   
+    G=fft[:,:,1].real/fft[:,:,0].real
+    G=np.nan_to_num(G, nan=0.0)
+    # plt.figure()
+    # plt.imshow(G)
+    # # plt.axis('off')
+    # plt.title('G')
+    # plt.colorbar()
+   
+    S=fft[:,:,1].imag/fft[:,:,0].real
+    S=np.nan_to_num(S, nan=0.0)
+    # plt.figure()
+    # plt.imshow(S)
+    # # plt.axis('off')
+    # plt.title('S')
+    # plt.colorbar()
+   
+    Ph=np.arctan2(S[:,:], G[:,:])+np.pi
+    Ph=np.nan_to_num(Ph, nan=0.0)
+    # plt.figure()
+    # plt.imshow(Ph)
+    # # plt.axis('off')
+    # plt.title('Phase')
+    # plt.colorbar()
+   
+    Mod=np.sqrt(G**2+S**2)
+    Mod=np.nan_to_num(Mod, nan=0.0)
+    # plt.figure()
+    # plt.imshow(Mod)
+    # # plt.axis('off')
+    # plt.title('Mod')
+    # plt.colorbar()
+   
+   
+    return G, S, Ph, Mod
 
 img = cv.imread('Images/image0.jpg')
 
