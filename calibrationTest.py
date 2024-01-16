b261636192f75f7fcd97511fa4004db0e913c8c2
xiaoyihua <dan-wang@live.com>
2024-01-15 21:48:04 -0800

    added approximate high/low hsvs for mask isolate

--- calibrationTest.py
+++ calibrationTest.py
@@ -15,6 +15,8 @@ import os
 class Image():
     def __init__(self, img, low=None, high=None):
         self.img = img
+        self.isolate = None
+        self.mask = None
         self.G = None
         self.S = None
         self.Mod = None
@@ -83,7 +85,7 @@ class Image():
         # Using inRange method, to create a mask
         self.mask = cv.inRange(self.img, self.low, self.high)
 
-    def isolate(self):
+    def setisolate(self):
         self.isolate = cv.bitwise_and(self.img, self.img, mask=self.mask)
 
 def main():
@@ -110,23 +112,23 @@ def main():
     # Remember that in HSV space, Hue is color from 0..180. Red 320-360, and 0 - 30.
     # We keep Saturation and Value within a wide range but note not to go too low or we start getting black/gray
     # set HSV lows and highs for each color
-    red650 = Image(img_hsv, low=np.array([0,0,0]), high=np.array([10,255,255]))
-    red625 = Image(img_hsv, low=, high=)
-    red610 = Image(img_hsv, low=, high=)
-    yellow600 = Image(img_hsv, low=, high=)
-    yellow580 = Image(img_hsv, low=, high=)
-    yellow575 = Image(img_hsv, low=, high=)
-    green550 = Image(img_hsv, low=, high=)
-    green540 = Image(img_hsv, low=, high=)
-    green525 = Image(img_hsv, low=, high=)
-    green500 = Image(img_hsv, low=, high=)
-    blue475 = Image(img_hsv, low=, high=)
-    blue450 = Image(img_hsv, low=, high=)
-    blue440 = Image(img_hsv, low=, high=)
-    blue430 = Image(img_hsv, low=, high=)
-    blue420 = Image(img_hsv, low=, high=)
-    violet410 = Image(img_hsv, low=, high=)
-    violet400 = Image(img_hsv, low=, high=)
+    red650 = Image(img_hsv, low=np.array([170,0,0]), high=np.array([180,255,255]))
+    red625 = Image(img_hsv, low=np.array([0,0,0]), high=np.array([5,255,255]))
+    red610 = Image(img_hsv, low=np.array([10,0,0]), high=np.array([15,255,255]))
+    yellow600 = Image(img_hsv, low=np.array([15,0,0]), high=np.array([25,255,255]))
+    yellow580 = Image(img_hsv, low=np.array([25,0,190]), high=np.array([29,255,255]))
+    yellow575 = Image(img_hsv, low=np.array([29,0,0]), high=np.array([45,255,255]))
+    green550 = Image(img_hsv, low=np.array([45,0,0]), high=np.array([55,255,255]))
+    green540 = Image(img_hsv, low=np.array([55,0,0]), high=np.array([70,90,255]))
+    green525 = Image(img_hsv, low=np.array([60,88,0]), high=np.array([70,255,255]))
+    green500 = Image(img_hsv, low=np.array([70,0,0]), high=np.array([100,255,255]))
+    blue475 = Image(img_hsv, low=np.array([100,0,0]), high=np.array([105,255,255]))
+    blue450 = Image(img_hsv, low=np.array([105,0,0]), high=np.array([111,150,150]))
+    blue440 = Image(img_hsv, low=np.array([112,0,0]), high=np.array([112,200,200]))
+    blue430 = Image(img_hsv, low=np.array([113,0,0]), high=np.array([115,150,150]))
+    blue420 = Image(img_hsv, low=np.array([115,0,0]), high=np.array([120,150,150]))
+    violet410 = Image(img_hsv, low=np.array([120,0,0]), high=np.array([130,150,150]))
+    violet400 = Image(img_hsv, low=np.array([130,0,0]), high=np.array([160,150,150]))
 
     # masks
     red650.setmask()
@@ -147,11 +149,32 @@ def main():
     violet410.setmask()
     violet400.setmask()
 
-    cv.imshow('isolate', forest)
+    # set isolates
+    red650.setisolate()
+    red625.setisolate()
+    red610.setisolate()
+    yellow600.setisolate()
+    yellow580.setisolate()
+    yellow575.setisolate()
+    green550.setisolate()
+    green540.setisolate()
+    green525.setisolate()
+    green500.setisolate()
+    blue475.setisolate()
+    blue450.setisolate()
+    blue440.setisolate()
+    blue430.setisolate()
+    blue420.setisolate()
+    violet410.setisolate()
+    violet400.setisolate()
+
     # Show image
+    # cv.imshow('red625isolate', violet400.isolate)
     # syntax: Picture name, variable
-    cv.imshow('Median', median)
+    # cv.imshow('Median', median)
     # cv.imshow('Gaussian', gauss)
+
+    # Calculate phasors and plot
 
     cv.waitKey(0)
 
