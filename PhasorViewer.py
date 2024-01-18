a012b986b33f8b329abf04359e6525a441c6f145
Daniel Wang <dan-wang@live.com>
2024-01-18 09:02:00 -0800

    baseline code for gui

--- PhasorViewer.py
+++ PhasorViewer.py
@@ -4,5 +4,21 @@ import tifffile as tiff
 import matplotlib.pyplot as plt
 from calibrationTest import *
 import os
+import PySimpleGUI as sg
 
 # GUI wrapper for viewing phasors
+
+# First the window layout in 2 columns
+
+file_list_column = [
+    [
+        sg.Text("Image Folder"),
+        sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
+        sg.FolderBrowse(),
+    ],
+    [
+        sg.Listbox(
+            values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
+        )
+    ],
+]
